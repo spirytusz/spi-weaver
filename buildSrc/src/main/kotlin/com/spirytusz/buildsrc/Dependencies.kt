2dcd606ccc23d4c2492b22514c99b91c08c4452a
zkw012300 <zhangwel261717@gmail.com>
2022-06-12 16:27:24 +0800

    微调demo，dependencies版本+1

--- buildSrc/src/main/kotlin/com/spirytusz/buildsrc/Dependencies.kt
+++ buildSrc/src/main/kotlin/com/spirytusz/buildsrc/Dependencies.kt
@@ -14,7 +14,7 @@ object Versions {
     const val common_codec = "1.15"
     const val common_io = "2.6"
     const val booster = "1.3.1"
-    const val spi_weaver = "0.0.1"
+    const val spi_weaver = "0.0.2"
 }
 
 object Dependencies {
