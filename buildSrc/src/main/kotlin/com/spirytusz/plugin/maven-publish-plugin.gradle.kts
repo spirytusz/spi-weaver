77933e1573a66501b7bb29b69f770a7b8a37aea6
zkw012300 <zhangwel261717@gmail.com>
2022-06-12 10:44:54 +0800

    升级GsonBooster版本，编译产物目标版本1.8

--- buildSrc/src/main/kotlin/com/spirytusz/plugin/maven-publish-plugin.gradle.kts
+++ buildSrc/src/main/kotlin/com/spirytusz/plugin/maven-publish-plugin.gradle.kts
@@ -9,6 +9,9 @@ plugins {
 java {
     withJavadocJar()
     withSourcesJar()
+
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
 }
 
 tasks.javadoc {
